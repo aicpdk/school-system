2473ca4388c5c9dfe826d0af97246d761f582f26
Mohammad Hariri <moh.050196@gmail.com>
2022-11-07 10:29:51 +0100

    Feature/sentry (#2)
    * redo
    
    * setup sentry
    
    * fix environment variable names

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,7 +4,7 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN_URI || process.env.NEXT_PUBLIC_SENTRY_DSN_URI;
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
 const DEBUG = process.env.SENTRY_DEBUG === 'true';
 
